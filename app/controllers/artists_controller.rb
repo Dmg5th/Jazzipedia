f764c94c7c526771628fcfa2ef076a5ab6e6fa9c
Dmg5th <dmgould310@gmail.com>
2021-01-25 14:27:53 -0500

    Persisted data to database

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,13 @@
 class ArtistsController < ApplicationController
 
     def index
+        artists = Artist.all
+        render json: artists
     end 
     
 end
+
+
+# render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
+
+# render json: sighting.to_json(include: [:bird, :location])
