dfe3e74808efa405236e09329eb48a124a8ebc82
Dmg5th <dmgould310@gmail.com>
2021-01-25 12:14:45 -0500

    Migrated first database and test seed data for artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Artist.create(name: "Miles Davis")
+Artist.create(name: "John Coltrane")
+Artist.create(name: "Hank Mobley")
