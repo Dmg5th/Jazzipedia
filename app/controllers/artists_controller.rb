f93355a1ac1bea634802a0f87efc7d1671cae750
Dmg5th <dmgould310@gmail.com>
2021-01-27 16:03:06 -0500

    included eras on create and index controllers

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,23 +2,23 @@ class ArtistsController < ApplicationController
 
     def index
         artists = Artist.all
-        render json: artists, include: :era, :status => 200
+        render json: artists, :include => :era, :status => 200
     end 
 
     def create 
        era = Era.find_by(name: params[:era])
        artist = Artist.create(name: params[:name], era: era)
-       render json: artist, :status => 201
+       render json: artist, :include => :era, :status => 201
     end 
 
     def show
         artist = Artist.find(params[:id])
-        render json: artists, include: :era
+        render json: artists, include: :era, :status => 200
     end 
     
     def destroy
         # Artist.find(params[:id]).destroy
-        # render :json => {id: params[:id], message: "Record was successfully deleted"}
+        # render :json => {id: params[:id], message: "Record was successfully deleted"}, :status => 200
       
          
         
