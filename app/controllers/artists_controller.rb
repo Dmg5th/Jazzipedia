b2fbf8af8f746f10a7fd12da8224d9733a56bcb0
Dmg5th <dmgould310@gmail.com>
2021-01-27 11:10:22 -0500

    Added pry dependancy

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,10 +4,21 @@ class ArtistsController < ApplicationController
         artists = Artist.all
         render json: artists, include: :era
     end 
+
+    def show
+        artist = Artist.find(params[:id])
+        render json: artists, include: :era
+    end 
     
     def destroy
         Artist.find(params[:id]).destroy
-        render :json => {id: params[:id]}
+        render :json => {id: params[:id], message: "Record was successfully deleted"}
+        binding.pry
+        # if artist.destroy
+        #     render :json => {id: params[:id], message: "Record was successfully deleted"}
+        # else
+        #     render :json => {message: "there was an error"}
+        # end 
     end 
 end
 
