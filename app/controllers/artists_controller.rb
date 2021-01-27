6b9a4d6bf16d2ec542b2c4bfbd329724111fb55c
Dmg5th <dmgould310@gmail.com>
2021-01-27 14:50:47 -0500

    Added status codes to ensure that data was correctly processing through the API

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,13 +2,13 @@ class ArtistsController < ApplicationController
 
     def index
         artists = Artist.all
-        render json: artists, include: :era
+        render json: artists, include: :era, :status => 200
     end 
 
     def create 
        era = Era.find_by(name: params[:era])
        artist = Artist.create(name: params[:name], era: era)
-       render json: artist, include: :era
+       render json: artist, :status => 201
     end 
 
     def show
